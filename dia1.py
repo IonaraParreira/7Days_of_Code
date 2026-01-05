f0ce0bb4c7de12f1acc4e28cd840c698bef0ae6d
Ionara <ionara.parreira@gmail.com>
2026-01-05 14:35:40 -0300

    oficial

---
+++ dia1.py
@@ -0,0 +1,11 @@
+import requests # Usando o módulo requests
+
+# Exigência 1: Executar requisição do tipo GET
+url = "https://last-airbender-api.fly.dev/api/v1/characters"
+resposta = requests.get(url)
+
+# Exigência 2: Pegar a resposta em JSON
+dados = resposta.json()
+
+# Exigência 3: Imprimir o corpo da resposta
+print(dados)

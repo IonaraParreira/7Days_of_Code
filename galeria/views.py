e78cd65961cf008197db39086a5d0bc74be40ced
Ionara <ionara.parreira@gmail.com>
2026-01-08 14:15:11 -0300

    Atualização

--- galeria/views.py
+++ galeria/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    # Criamos uma lista [] que contém vários dicionários {}
+    personagens_lista = [
+        {'nome': 'Aang', 'elemento': 'Ar'},
+        {'nome': 'Katara', 'elemento': 'Água'},
+        {'nome': 'Sokka', 'elemento': 'Guerreiro'},
+        {'nome': 'Toph', 'elemento': 'Terra'}
+    ]
+    
+    # O contexto leva a lista para o HTML com o nome 'lista_avatar'
+    return render(request, 'galeria/index.html', {'lista_avatar': personagens_lista})

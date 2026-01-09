0ca65a75a9bdf0fec630d74906b6d4b9fcbc7eab
Ionara <ionara.parreira@gmail.com>
2026-01-08 22:20:23 -0300

    Atualização

--- galeria/views.py
+++ galeria/views.py
@@ -1,13 +1,31 @@
+import requests
 from django.shortcuts import render
+from googletrans import Translator
 
 def index(request):
-    # Criamos uma lista [] que contém vários dicionários {}
-    personagens_lista = [
-        {'nome': 'Aang', 'elemento': 'Ar'},
-        {'nome': 'Katara', 'elemento': 'Água'},
-        {'nome': 'Sokka', 'elemento': 'Guerreiro'},
-        {'nome': 'Toph', 'elemento': 'Terra'}
-    ]
+    url = 'https://last-airbender-api.fly.dev/api/v1/characters'
+    personagens_traduzidos = []
     
-    # O contexto leva a lista para o HTML com o nome 'lista_avatar'
-    return render(request, 'galeria/index.html', {'lista_avatar': personagens_lista})
+    try:
+        #📡 Chamando a API do Avatar
+        resposta = requests.get(url)
+        if resposta.status_code == 200:
+            personagens = resposta.json()
+            tradutor = Translator()
+
+            # Pegando os 10 primeiros para ser mais rápido
+            for p in personagens[:10]:
+                nome_br = tradutor.translate(p['name'], dest='pt').text
+                afiliacao = p.get('affiliation', 'Sem afiliação')
+                afiliacao_br = tradutor.translate(afiliacao, dest='pt').text
+
+                personagens_traduzidos.append({
+                    'nome': nome_br,
+                    'elemento': afiliacao_br})
+                
+    except Exception as e:    
+        print(f"Erro de conexão: {e}")
+
+    # Enviando a lista real para o seu HTML
+    return render(request, 'galeria/index.html', {'lista_avatar': personagens_traduzidos})
+        
